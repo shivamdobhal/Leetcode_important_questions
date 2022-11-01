624a68e19af4cde84105cd509b5e37a61c9fdbb3
Shiv@m Dobh@l <73071952+Shivamdobhal@users.noreply.github.com>
2022-11-01 21:12:21 +0530

    Update 73_Set MatrixZeroes.cpp

--- matrix_problems/73_Set MatrixZeroes.cpp
+++ matrix_problems/73_Set MatrixZeroes.cpp
@@ -19,4 +19,41 @@ Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 
 solution---whereever you find 0 make that column and row 0
-  
+            approach---make two dummy array for row and one for column.
+   class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+            int row=matrix.size();                   //calculate row size
+            int col=matrix[0].size();                 //calculate column size
+          
+          vector < int > arr1(row,-1);              //dummy vector for row
+          vector  <int> arr2(col,-1);                 //dummy vector for column
+            //loop to enter 0 in the two dummy vector
+                for(int i=0;i<row;i++)                    
+            {
+                    for(int j=0;j<col;j++)
+                    {
+                            if(matrix[i][j]==0)
+                            {
+                                    arr1[i]=0;
+                                    arr2[j]=0;
+                            }
+                    }
+            }
+            
+               //loops for sending values to matrix[][]
+             for(int i=0;i<row;i++)
+            {
+                    for(int j=0;j<col;j++)
+                    {
+                            if(arr2[j]==0 || arr1[i]==0)
+                            {
+                                    matrix[i][j]=0;
+                            }
+                            
+                    }
+             }
+            
+     
+    }
+};
