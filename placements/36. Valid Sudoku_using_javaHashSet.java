308f45ce123f4b477a7c8d6e2acac6fab666b537
Shiv@m Dobh@l <73071952+Shivamdobhal@users.noreply.github.com>
2022-11-05 15:17:28 +0530

    Create 36. Valid Sudoku_using_javaHashSet.java

---
+++ placements/36. Valid Sudoku_using_javaHashSet.java
@@ -0,0 +1,27 @@
+//using single iteration in matrix
+//amazon question
+//first check for row ,then column and then in boxes
+//java hashset
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+       HashSet set=new HashSet();
+        
+            for(int i=0;i<9;i++)
+            {
+                   for(int j=0;j<9;j++)
+            {
+                    if(board[i][j]!='.')
+                    {
+                            if(!set.add("row"+i+board[i][j]))
+                                    return false;
+                            if(!set.add("column"+j+board[i][j]))
+                                    return false;
+                            if(!set.add("box"+(i/3)*3+j/3+board[i][j]))
+                                    return false;
+                    }
+            } 
+            }
+            return true;
+    }
+}
