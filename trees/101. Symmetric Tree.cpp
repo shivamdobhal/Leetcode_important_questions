5913414adf5cadd85f2e345aaea93c12dbb303e4
Shiv@m Dobh@l <73071952+Shivamdobhal@users.noreply.github.com>
2022-11-13 20:34:06 +0530

    Update 101. Symmetric Tree.cpp

--- trees/101. Symmetric Tree.cpp
+++ trees/101. Symmetric Tree.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+        
+        bool result(TreeNode* root1,TreeNode* root2)
+        {
+        if(root1==NULL && root2==NULL) return true;
+        if(root1==NULL || root2==NULL) return false;
+        if(root1->val!=root2->val)     return false;        
+        
+        return result(root1->left,root2->right) &&   result(root1->right,root2->left);      
+        }
+        
+    bool isSymmetric(TreeNode* root) {
+        if(root==NULL)
+                return true;
+            
+          return result(root->left,root->right);  
+    }
+};
