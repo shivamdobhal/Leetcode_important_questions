e076f79132ebf7bb2e5ab27c39190b8ea4472e88
Shiv@m dobh@l <73071952+shivamdobhal@users.noreply.github.com>
2023-01-30 22:04:00 +0530

    Update 198. House Robber.cpp

--- DP/198. House Robber.cpp
+++ DP/198. House Robber.cpp
@@ -1,3 +1,23 @@
 MEDIUM:-
   
-  
+  class Solution {
+public:
+    int rob(vector<int>& nums) {
+        //tablulation method
+            if(nums.size()==1)
+                    return nums[0];
+            
+            vector<int> dp(nums.size(),0);
+            dp[0]=nums[0];
+            dp[1]=max(dp[0],nums[1]);
+            
+            
+            for(int i=2;i<nums.size();i++)
+            {
+                    dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+                                        
+            }
+            int n=nums.size();
+            return dp[n-1];
+    }
+};
