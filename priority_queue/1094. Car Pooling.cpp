78b38a6853b5a2e373a3832a530f2286bc502b53
Shiv@m dobh@l <73071952+shivamdobhal@users.noreply.github.com>
2023-02-26 13:30:56 +0530

    Update 1094. Car Pooling.cpp

--- priority_queue/1094. Car Pooling.cpp
+++ priority_queue/1094. Car Pooling.cpp
@@ -1,1 +1,29 @@
-v
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        unordered_map<int,int> map;
+
+        for(int i=0;i<roads.size();i++)
+        {
+            map[roads[i][0]]++;
+            map[roads[i][1]]++;
+        }
+       long long ans=0;
+        priority_queue< long long> pq;
+        
+            for(auto it :map)
+            {
+                    pq.push(it.second);
+            }
+            
+        while(!pq.empty())
+        {
+                ans=ans+pq.top()*n;
+                pq.pop();
+                n--;
+                
+        }
+            
+        return ans;
+    }
+};
