fca341b5b2109d587428baf7cb1f974093f03ad0
Shiv@m dobh@l <73071952+shivamdobhal@users.noreply.github.com>
2023-03-01 23:03:25 +0530

    Update and rename about priority queue to about priority queue.cpp

---
+++ priority_queue/about priority queue.cpp
@@ -0,0 +1,8 @@
+priority queue  is a stack like structure but it an queue in which when we push the elements it gets sorted automatically.
+
+min heap---------use where we have to find  kth max. value /largest value
+            syntax= priority_queue<int,vector<int>,greater<int>> pq;
+            
+            
+max heap---------use where we have to find  kth min. value /smallest value
+           syntax=priority_queue<int> pq;
